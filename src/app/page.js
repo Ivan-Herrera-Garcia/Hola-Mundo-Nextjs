bdecea0da8d7fb7709679a689e4b5208980f3531
Ivan Herrera (Bellkhen) <herreraleon14@gmail.com>
2023-06-30 18:00:21 -0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,130 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-700">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Con amor 🥰, hecho por:&nbsp;
+          <code className="font-mono font-bold">Ivan HG</code>
+        </div>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+            
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark"
+          src="/logo.svg"
+          alt="Logo React"
+          width={180}
+          height={37}
+          priority
+        /> 
+        <div className='mb-2 font-semibold w-20 text-6xl grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
+          <span className="">
+           +
+          </span> 
+      </div> 
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark"
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://github.com/Ivan-Herrera-Garcia"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            GitHub{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Podras encontrar proyectos personales y estudiantiles en mi espacio en GitHub.
+          </p>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/ivan-herrera-garcia-8b64411b8/"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Linkedin{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Para comunicacion o para colaboraciones, no dudes en contactarme!
+          </p>
+        </a>
+
+        <a
+          href="https://mi-portafolio-ihg.netlify.app/"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Mi pequeño portafolio{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Un pequeño espacio que hace uso de la pila MERN
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/RETBOT/ChatDynamix"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Tu propio ChatGPT en local{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <a href = "https://github.com/retbot" >
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            En colaboración con de Retbot hicimos un modelo de chatbot haciendo uso solo de tu procesador 
+            de modo local en tu maquina.
+          </p>
+          </a>
+        </a>
+      </div>
+    </div>
+  )
+}
