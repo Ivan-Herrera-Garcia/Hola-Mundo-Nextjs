4fe670c9eddef4ad1ee050595f5b2f77a18a82c0
Ivan Herrera (Bellkhen) <herreraleon14@gmail.com>
2023-07-03 09:37:29 -0600

    Actualizacion

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-700">
+    <div className="flex-1 flex-col justify-between p-28 bg-slate-700 border h-full w-full">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Con amor 🥰, hecho por:&nbsp;
@@ -31,14 +31,14 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark hidden sm:block"
           src="/logo.svg"
           alt="Logo React"
           width={180}
           height={37}
           priority
         /> 
-        <div className='mb-2 font-semibold w-20 text-6xl grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
+        <div className='mb-2 font-semibold w-20 text-6xl grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left  hidden sm:block'>
           <span className="">
            +
           </span> 
@@ -117,12 +117,12 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <a href = "https://github.com/retbot" >
+          
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             En colaboración con de Retbot hicimos un modelo de chatbot haciendo uso solo de tu procesador 
             de modo local en tu maquina.
           </p>
-          </a>
+          
         </a>
       </div>
     </div>
